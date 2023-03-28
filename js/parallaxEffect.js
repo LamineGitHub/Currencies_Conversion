7ac3d4e136f66d19b0e89f67f430894e2514091f
Lamine Diallo <bah07565@gmail.com>
2023-03-28 04:26:46 +0100

    Optimization of the parallax effect

--- js/parallaxEffect.js
+++ js/parallaxEffect.js
@@ -6,37 +6,60 @@ const selectCountryFrom = document.getElementById("selectCountryFrom")
 const selectCountryTo = document.getElementById("selectCountryTo")
 const listCountry = document.getElementById("listCountry")
 
-/* Ajout d'un écouteur d'événement à l'élément iconCalculator. Lorsque l'élément iconCalculator est
-cliqué, l'élément calculatorSection aura la classe calcBoxShadow qui lui sera ajoutée. Ensuite,
-après 500 millisecondes, la classe right-[101%] sera ajoutée à l'élément calculatorSection et la
-classe right-0 sera supprimée de l'élément calculatorSection. La classe -translate-x-6 sera
-également ajoutée à l'élément convertSection. */
-iconCalculator.addEventListener("click", () => {
-  calculatorSection.classList.add("calcBoxShadow")
+/**
+ * Ajoutez la classe calcBoxShadow à la section, puis après un délai, ajoutez la showClass et supprimez
+ * la hideClass, et basculez la classe -translate-x-6 sur la convertSection.
+ * @param {string} section - la section que vous souhaitez basculer
+ * @param {string} hideClass - la classe qui cache la section
+ * @param {string} showClass - la classe qui sera ajoutée à la section qui sera affichée
+ * @param {number} delay - le temps qu'il faut pour que l'animation démarre
+ */
+const toggleSection = (section, hideClass, showClass, delay) => {
+  section.classList.add("calcBoxShadow")
   setTimeout(() => {
-    calculatorSection.classList.add("right-[101%]")
-    calculatorSection.classList.remove("right-0")
-    convertSection.classList.add("-translate-x-6")
-  }, 500)
+    section.classList.add(showClass)
+    section.classList.remove(hideClass)
+    convertSection.classList.toggle("-translate-x-6")
+  }, delay)
+}
+
+/**
+ * Ajout d'un écouteur d'événement à l'élément iconCalculator.
+ * Lorsque l'élément iconCalculator est cliqué, la fonction toggleSection est appelée.
+ * La fonction toggleSection prend quatre arguments : calculatorSection, "right-0", "right-[101%]" et 500.
+ * L'argument calculatorSection est l'élément calculatorSection.
+ * L'argument "right-0" est la classe qui cache l'élément calculatorSection.
+ * L'argument "right-[101%]" est la classe qui affiche l'élément calculatorSection.
+ * L'argument 500 est le délai en millisecondes avant que la fonction toggleSection ne soit appelée.
+ */
+iconCalculator.addEventListener("click", () => {
+  toggleSection(calculatorSection, "right-0", "right-[101%]", 500)
 })
 
-/* Un écouteur d'événement qui écoute un clic sur l'élément iconBack. Lorsque l'élément iconBack est
-cliqué, l'élément calculatorSection reçoit la classe right-0 et la classe right-[101%] est
-supprimée.La classe -translate-x-6  sera supprimée de l'élément convertSection et la classe calcBoxShadow est
-supprimée de l'élément calculatorSection après 500 millisecondes. */
+/**
+ *  Ajout d'un écouteur d'événement à l'élément iconBack. Lorsque l'élément iconBack est cliqué, la fonction toggleSection est appelée.
+ * La fonction toggleSection prend quatre arguments : calculatorSection, "right-[101%]", "right-0" et 0.
+ * L'argument calculatorSection est l'élément calculatorSection.
+ * L'argument "right-[101%]" est la classe qui cache l'élément calculatorSection.
+ * L'argument "right-0" est la classe qui affiche l'élément calculatorSection.
+ * L'argument 0 est le délai en millisecondes avant que la fonction toggleSection ne soit appelée.
+ */
 iconBack.addEventListener("click", () => {
-  calculatorSection.classList.add("right-0")
-  calculatorSection.classList.remove("right-[101%]")
-  convertSection.classList.remove("-translate-x-6")
+  toggleSection(calculatorSection, "right-[101%]", "right-0", 0)
   setTimeout(() => {
     calculatorSection.classList.remove("calcBoxShadow")
   }, 500)
 })
 
-/* Ajout d'un écouteur d'événement à l'élément selectCountryFrom. Lorsque l'élément est cliqué, il
-ajoute la classe "right-0" à l'élément listCountry et supprime la classe "right-[100%]" de l'élément
-listCountry. */
-selectCountryFrom.addEventListener("click", () => {
-  listCountry.classList.add("right-0")
-  listCountry.classList.remove("right-[100%]")
-})
+/**
+ * Si la liste de classes de l'élément avec l'id "listCountry" contient la classe "right-0", alors
+ * supprimez-la et ajoutez la classe "right-[100%]". Sinon, supprimez la classe "right-[100%]" et
+ * ajoutez la classe "right-0".
+ */
+function toggleListCountry() {
+  listCountry.classList.toggle("right-0")
+  listCountry.classList.toggle("right-[100%]")
+}
+
+selectCountryFrom.addEventListener("click", toggleListCountry)
+selectCountryTo.addEventListener("click", toggleListCountry)
